b2f541c758a7a9adf4d61c59984a96e601e1238d
Carlos <crodas035@gmail.com>
2021-02-06 18:09:27 +0100

    mejora

--- routes/api.php
+++ routes/api.php
@@ -3,29 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-
-// Route::get('/users/{username}/', 'App\Http\Controllers\UserController@find');
-
-//Rutas usuario
-// Route::get('/user/{username}/', 'App\Http\Controllers\UserController@findUser');
-// //Rutas contenedores
-// Route::get('/container', 'App\Http\Controllers\ContainerController@index');
 
 Route::get('/container/{street_name}/', 'App\Http\Controllers\ContainerController@findContainerByName');
 
